dd027d62fee229cdb1c9056559f2f380090d312b
Usmaann <usmaannn912@gmail.com>
2024-01-07 22:03:17 +0100

    new features added

--- app/src/main/java/com/sirwhite/mehndi/MainActivity.java
+++ app/src/main/java/com/sirwhite/mehndi/MainActivity.java
@@ -1,14 +1,98 @@
 package com.sirwhite.mehndi;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout start;
+    LinearLayout share, rate;
+
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(1024, 1024);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.rel_start);
+
+        rate = findViewById(R.id.li_rate);
+
+
+        start.setOnClickListener(view -> {
+
+
+            if (checkPermission(MainActivity.this)) {
+                startActivity(new Intent(MainActivity.this, MainContent.class));
+
+            } else {
+                requestPermission(MainActivity.this, 100);
+            }
+        });
+
+        rate.setOnClickListener(view -> {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+            }
+        });
+
+       /* share.setOnClickListener(view -> {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey check out my app at: https://play.google.com/store/apps/details?id=" + getPackageName());
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        });*/
     }
+
+
+    public static boolean checkPermission(Context context) {
+        int result1 = ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE);
+        int result2 = ContextCompat.checkSelfPermission(context, READ_EXTERNAL_STORAGE);
+
+
+        return result1 == PackageManager.PERMISSION_GRANTED &&
+                result2 == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    public static void requestPermission(Activity context, int code) {
+        ActivityCompat.requestPermissions(context, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, code);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "double tap to exit!", Toast.LENGTH_SHORT).show();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
+
+    }
+
 }
